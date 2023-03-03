2f3b85f33100e8527eab2e16849766fb5a571dec
liulongling <1805348347@qq.com>
2023-03-03 10:23:04 +0800

    fix:ETF不计税费

--- backend/src/main/java/com/profit/commons/utils/BondUtils.java
+++ backend/src/main/java/com/profit/commons/utils/BondUtils.java
@@ -5,13 +5,15 @@ public class BondUtils {
 
     public static Double getTaxation(String plate, Double total, boolean isSell) {
         Double taxation = 0.000;
-        if (plate.equals("上证")) {
-            //过户费成交金额0.002%
-            taxation += total * 0.00002;
-        }
-        if (isSell) {
-            //印花税0.1%
-            taxation += total * 0.001;
+        if(!plate.equals("ETF")){
+            if (plate.equals("上证")) {
+                //过户费成交金额0.002%
+                taxation += total * 0.00002;
+            }
+            if (isSell) {
+                //印花税0.1%
+                taxation += total * 0.001;
+            }
         }
         //券商佣金
         double zqyj= total * 0.00001;

c1ff659bb763ac8cb199d16b54a1fab6b27c2945
liulongling <1805348347@qq.com>
2023-03-18 09:12:39 +0800

    feat:交易记录界面新增备注字段

--- backend/src/main/java/com/profit/base/domain/BondBuyLog.java
+++ backend/src/main/java/com/profit/base/domain/BondBuyLog.java
@@ -1,7 +1,10 @@
 package com.profit.base.domain;
 
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class BondBuyLog {
     /**
      * 唯一ID
@@ -59,103 +62,12 @@ public class BondBuyLog {
     private Date operTime;
 
     /**
+     * 备注
+     */
+    private String remarks;
+
+    /**
      * 创建时间
      */
     private Date createTime;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getGpId() {
-        return gpId;
-    }
-
-    public void setGpId(String gpId) {
-        this.gpId = gpId == null ? null : gpId.trim();
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public Integer getCount() {
-        return count;
-    }
-
-    public void setCount(Integer count) {
-        this.count = count;
-    }
-
-    public Double getCost() {
-        return cost;
-    }
-
-    public void setCost(Double cost) {
-        this.cost = cost;
-    }
-
-    public Byte getType() {
-        return type;
-    }
-
-    public void setType(Byte type) {
-        this.type = type;
-    }
-
-    public Integer getSellCount() {
-        return sellCount;
-    }
-
-    public void setSellCount(Integer sellCount) {
-        this.sellCount = sellCount;
-    }
-
-    public Double getSellIncome() {
-        return sellIncome;
-    }
-
-    public void setSellIncome(Double sellIncome) {
-        this.sellIncome = sellIncome;
-    }
-
-    public String getBuyDate() {
-        return buyDate;
-    }
-
-    public void setBuyDate(String buyDate) {
-        this.buyDate = buyDate == null ? null : buyDate.trim();
-    }
-
-    public Byte getStatus() {
-        return status;
-    }
-
-    public void setStatus(Byte status) {
-        this.status = status;
-    }
-
-    public Date getOperTime() {
-        return operTime;
-    }
-
-    public void setOperTime(Date operTime) {
-        this.operTime = operTime;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
 }
